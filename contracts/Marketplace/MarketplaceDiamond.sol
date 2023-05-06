4536388d0f2d9b78a01a070aabb5bd50c47a927d
Sean P <seanp@seans-mbp-2.mynetworksettings.com>
2023-05-06 08:19:42 -0400

    fix - re-added abicoder

--- contracts/Marketplace/MarketplaceDiamond.sol
+++ contracts/Marketplace/MarketplaceDiamond.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.1;
+pragma abicoder v2;
 
 import "diamond-2/contracts/libraries/LibDiamond.sol";
 import "diamond-2/contracts/interfaces/IDiamondLoupe.sol";
